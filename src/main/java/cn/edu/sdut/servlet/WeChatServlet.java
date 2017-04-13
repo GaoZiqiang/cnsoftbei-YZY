b3c44b3c9facd482eaa1857bb018913ed09ec52f
gaoziqiang <gaoziqianghi@.com>
2017-04-13 20:42:40 +0800

    项目代码

---
+++ src/main/java/cn/edu/sdut/servlet/WeChatServlet.java
@@ -0,0 +1,29 @@
+package cn.edu.sdut.servlet;
+
+/**
+*获取值
+*/
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import cn.edu.sdut.util.CheckUtil;
+
+public class WeChatServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String signature = req.getParameter("signature");
+		String timestamp = req.getParameter("timestamp");
+		String nonce = req.getParameter("nonce");
+		String echostr = req.getParameter("echostr");
+
+		// 调用逻辑验证
+		PrintWriter out = resp.getWriter();
+		if (CheckUtil.checkSignature(signature, timestamp, nonce)) {
+			out.println(echostr);
+		}
+	}
+}
