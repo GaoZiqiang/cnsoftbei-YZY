2bfb3c305cf04552e9f6d27259dea5a795f3ed40
gaoziqiang <gaoziqianghi@.com>
2017-04-14 22:39:37 +0800

    添加MessageUtil

---
+++ src/main/java/cn/edu/sdut/util/MessageUtil.java
@@ -0,0 +1,42 @@
+package cn.edu.sdut.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+public class MessageUtil {
+	/**
+	 * 
+	 * xml转换成Map
+	 * */
+	public static Map<String, String> xmlToMap(HttpServletRequest request) throws IOException, DocumentException {
+		Map<String, String> map = new HashMap<String, String>();
+		SAXReader reader = new SAXReader();
+
+		InputStream ins = request.getInputStream();
+		Document doc = reader.read(ins);
+
+		Element root = doc.getRootElement();
+
+		List<Element> list = root.elements();
+
+		for (Element e : list) {
+			map.put(e.getName(), e.getText());
+		}
+		ins.close();
+		return map;
+	}
+	
+	public static String textMessageToXml(TextMessage textMessage) {
+		
+	}
+}
