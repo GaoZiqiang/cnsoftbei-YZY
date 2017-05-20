f49a0862f0481e4fae63346799450e2b8fbbfd0d
gaoziqiang <gaoziqianghi@163.com>
2017-05-20 16:49:37 +0800

    controller doPost方法

--- src/main/java/cn/edu/sdut/controller/WeChatServletController.java
+++ src/main/java/cn/edu/sdut/controller/WeChatServletController.java
@@ -5,14 +5,18 @@ package cn.edu.sdut.controller;
 */
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import cn.edu.sdut.msg.TextMessage;
 import cn.edu.sdut.service.CheckUtil;
 import cn.edu.sdut.service.CoreService;
+import cn.edu.sdut.service.MessageUtil;
 
 public class WeChatServletController extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -31,18 +35,25 @@ public class WeChatServletController extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		 // 将请求、响应的编码均设置为UTF-8（防止中文乱码）  
-        request.setCharacterEncoding("UTF-8");  
-        response.setCharacterEncoding("UTF-8");  
-        PrintWriter out = response.getWriter();// 获取resq的数据流
-  
-        // 调用核心业务类接收消息、处理消息  
-        String respMessage = CoreService.processRequest(request);  
-          
-        // 响应消息  
-        
-        out.print(respMessage);  
-        out.close();  
+		try {
+			Map<String,String> map = MessageUtil.xmlToMap(request);
+			String fromUserName = map.get("FromUserName");
+			String toUserName = map.get("ToUserName");
+			String msgType = map.get("MsgType");
+			String content = map.get("Content");
+			
+			if("text".equals(msgType)) {
+				TextMessage text = new TextMessage();
+				text.setFromUserName(toUserName);
+				text.setToUserName(fromUserName);
+				text.setMsgType("text");
+				text.setCreateTime(new Date().getTime());
+				text.setContent("您发送的消息是:" + content);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
    
 
 	}
