eca9a9c66496938f76f0e1df0b2537c3b591862d
gaoziqiang <gaoziqianghi@163.com>
2017-05-22 10:57:58 +0800

    servletController signature参数

--- src/main/java/cn/edu/sdut/controller/WeChatServletController.java
+++ src/main/java/cn/edu/sdut/controller/WeChatServletController.java
@@ -18,16 +18,26 @@ import cn.edu.sdut.service.CheckUtil;
 import cn.edu.sdut.service.MessageUtil;
 
 public class WeChatServletController extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String signature = req.getParameter("signature");
+		String signature = req.getParameter("signature");//该signature来自微信服务器
 		String timestamp = req.getParameter("timestamp");
 		String nonce = req.getParameter("nonce");
 		String echostr = req.getParameter("echostr");
 
+		//打印输出signature
+		System.out.println("ServletController中signature:" + signature.toString());
+		System.out.println("ServletController中timestamp:" + timestamp.toString());
+		System.out.println("ServletController中nonce:" + nonce.toString());
+		System.out.println("ServletController中echostr:" + echostr.toString());
 		// 调用逻辑验证
 		PrintWriter out = resp.getWriter();
 		if (CheckUtil.checkSignature(signature, timestamp, nonce)) {
-			out.println(echostr);
+			out.println(echostr);//返回微信服务器
 		}
 		out.close();
 		out = null;
